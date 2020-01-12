a90ee29371a360695802461e2934cc171df3919a
rmaguire31 <rmaguire31@gmail.com>
2020-01-12 16:19:38 +0000

    Defined filter templates for DSP program, and updated fir.c/.h to be simpler

--- fir.c
+++ fir.c
@@ -1,11 +1,48 @@
-#include <stdlib.h>
 #include "fir.h"
 
+// Handy macro to find number of elements in an array
+#define ELEMOF(X) (sizeof(X)/sizeof(X[0]))
 
-float fir_filter(float *b, circ_buffer_t *x) {
-    float y = 0;
-    for (size_t i = 0; i < ELEMOF(x->samples); i++) {
-        y += b[i] * x->samples[(x->offset + i) % ELEMOF(x->samples)];
+
+// Return address of the next sample in the circular buffer
+float *fir_buffer_next(fir_buffer_t *buffer) {
+    float *p_sample = &buffer->samples[buffer->offset]
+    buffer->offset++;
+    return p_sample
+}
+
+
+// Return address of the next sample in the circular buffer
+float *fir_buffer_complex_next(fir_buffer_complex_t *buffer) {
+    float complex *p_sample = &buffer->samples[buffer->offset]
+    buffer->offset++;
+    return p_sample
+}
+
+
+// Convolve filter weights with circular buffer
+void fir_filter(
+    float weights[FIR_FILTER_LEN],
+    fir_buffer_t *input,
+    float *output
+) {
+    *output = 0
+    for (size_t i = 0; i < ELEMOF(weights); i++) {
+        size_t j = (input->offset + i) % ELEMOF(input->samples);
+        *output += weights[i] * input->samples[j];
     }
-    return y;
+}
+
+
+// Convolve filter weights with complex circular buffer
+void fir_filter_complex(
+    float weights[FIR_FILTER_LEN],
+    fir_buffer_complex_t *input,
+    float complex *output
+) {
+    *output = 0
+    for (size_t i = 0; i < ELEMOF(weights); i++) {
+        size_t j = (input->offset + i) % ELEMOF(input->samples);
+        *output += weights[i] * input->samples[j];
+    }
 }
--- fir.h
+++ fir.h
@@ -1,11 +1,38 @@
-#define ELEMOF(X) (sizeof(X)/sizeof(X[0]))
-#define FILTER_LEN (1024)
+#include <complex.h>
+#include <stdlib.h>
 
 
-typedef struct circ_buffer {
-    float samples[FILTER_LEN];
+// Handy buffer length macro, and a fixed filter length
+#define FIR_FILTER_LEN (1024)
+
+
+// Data structures
+typedef struct fir_buffer {
+	// Circular buffer
+    float samples[FIR_FILTER_LEN];
     size_t offset;
-} circ_buffer_t;
+} fir_buffer_t;
+
+typedef struct fir_buffer_complex {
+	// Complex circular buffer
+    float complex samples[FIR_FILTER_LEN];
+    size_t offset;
+} fir_buffer_complex_t;
 
 
-float fir_filter(float *b, circ_buffer_t *x);
+// Find address of next sample in buffer
+float *fir_buffer_next(fir_buffer_t *buffer);
+float complex *fir_buffer_complex_next(fir_buffer_complex_t *buffer);
+
+
+// Find output of FIR filter
+void fir_filter(
+	float weights[FIR_FILTER_LEN],
+	fir_buffer_t *inputs,
+	float *output
+);
+void fir_filter_complex(
+	float weights[FIR_FILTER_LEN],
+	fir_buffer_complex_t *inputs,
+	float complex *output
+);
