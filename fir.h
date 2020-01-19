2361eaba512b406f29df64991ad537e1520e2e92
lmiller1996 <58821699+lmiller1996@users.noreply.github.com>
2020-01-19 16:16:54 +0000

    Update fir.h

--- fir.h
+++ fir.h
@@ -1,4 +1,3 @@
-#include <complex.h>
 #include <stdlib.h>
 
 
@@ -7,32 +6,20 @@
 
 
 // Data structures
-typedef struct fir_buffer {
-	// Circular buffer
-    float samples[FIR_FILTER_LEN];
-    size_t offset;
-} fir_buffer_t;
-
-typedef struct fir_buffer_complex {
-	// Complex circular buffer
-    float complex samples[FIR_FILTER_LEN];
-    size_t offset;
-} fir_buffer_complex_t;
+struct fir_buffer {
+    // Circular buffer
+    float samples[FIR_FILTER_LEN]; // array with size equal to filter length
+    size_t offset; // index of newest sample within samples
+}; 
 
 
-// Find address of next sample in buffer
-float *fir_buffer_next(fir_buffer_t *buffer);
-float complex *fir_buffer_complex_next(fir_buffer_complex_t *buffer);
+// Find address of next sample in buffer to write
+float *fir_buffer_next(struct fir_buffer *p_buffer); // function that returns a pointer to a float
 
 
-// Find output of FIR filter
-void fir_filter(
-	float weights[FIR_FILTER_LEN],
-	fir_buffer_t *inputs,
-	float *output
-);
-void fir_filter_complex(
-	float weights[FIR_FILTER_LEN],
-	fir_buffer_complex_t *inputs,
-	float complex *output
+// Compute output of FIR filter by reading buffer and multiplying with weights
+void fir_filter( 
+    float weights[FIR_FILTER_LEN],
+    struct fir_buffer *p_buffer,
+    float *p_output
 );
