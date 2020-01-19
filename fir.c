91b41604117228e689fd34695b31df74906019ad
lmiller1996 <58821699+lmiller1996@users.noreply.github.com>
2020-01-19 16:16:59 +0000

    Update fir.c

--- fir.c
+++ fir.c
@@ -1,48 +1,26 @@
 #include "fir.h"
 
 // Handy macro to find number of elements in an array
-#define ELEMOF(X) (sizeof(X)/sizeof(X[0]))
+#define ELEMOF(X) (sizeof(X)/sizeof(X[0])) 
 
 
 // Return address of the next sample in the circular buffer
-float *fir_buffer_next(fir_buffer_t *buffer) {
-    float *p_sample = &buffer->samples[buffer->offset]
-    buffer->offset++;
-    return p_sample
+float *fir_buffer_next(struct fir_buffer *p_buffer) {
+    // Make all of the samples a bit newer
+    p_buffer->offset = (p_buffer->offset - 1) % ELEMOF(p_buffer->samples); // update offset value  
+    float *p_input = &(p_buffer->samples[p_buffer->offset]); // returns address of value to be overwritten with newest sample
+    return p_input;
 }
-
-
-// Return address of the next sample in the circular buffer
-float *fir_buffer_complex_next(fir_buffer_complex_t *buffer) {
-    float complex *p_sample = &buffer->samples[buffer->offset]
-    buffer->offset++;
-    return p_sample
-}
-
 
 // Convolve filter weights with circular buffer
 void fir_filter(
     float weights[FIR_FILTER_LEN],
-    fir_buffer_t *input,
-    float *output
+    struct fir_buffer *p_buffer,
+    float *p_output
 ) {
-    *output = 0
+    *p_output = 0;
     for (size_t i = 0; i < ELEMOF(weights); i++) {
-        size_t j = (input->offset + i) % ELEMOF(input->samples);
-        *output += weights[i] * input->samples[j];
-    }
-}
-
-
-// Convolve filter weights with complex circular buffer
-void fir_filter_complex(
-    float weights[FIR_FILTER_LEN],
-    fir_buffer_complex_t *input,
-    float complex *output
-) {
-    *output = 0
-    for (size_t i = 0; i < ELEMOF(weights); i++) {
-        size_t j = (input->offset + i) % ELEMOF(input->samples);
-        *output += weights[i] * input->samples[j];
+        size_t j = (p_buffer->offset + i) % ELEMOF(p_buffer->samples);
+        *p_output += weights[i] * p_buffer->samples[j];
     }
 }
