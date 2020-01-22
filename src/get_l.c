0116b4bbd4a0c0549061370d59228109cdadb160
lmiller1996 <58821699+lmiller1996@users.noreply.github.com>
2020-01-22 22:13:59 +0000

    Add files via upload

---
+++ src/get_l.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+
+float get_l(float *p_summ_res,  float *p_diff_res) {
+    float l = 0;
+    for (size_t i = 0; i < sizeof(sum_res); i++) {
+        l[i] = *p_sum_res[i] - *p_diff_res) / 2;
+    }
+    return l;
+}
