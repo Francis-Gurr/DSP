d0aba30b89fed709f6b1dcecf2d17e7fbdb4dedc
lmiller1996 <58821699+lmiller1996@users.noreply.github.com>
2020-01-22 22:19:22 +0000

    Update get_l.c

--- src/get_l.c
+++ src/get_l.c
@@ -3,7 +3,7 @@
 float get_l(float *p_summ_res,  float *p_diff_res) {
     float l = 0;
     for (size_t i = 0; i < sizeof(sum_res); i++) {
-        l[i] = *p_sum_res[i] - *p_diff_res) / 2;
+        l[i] = (*p_sum_res[i] - *p_diff_res) / 2;
     }
     return l;
 }
