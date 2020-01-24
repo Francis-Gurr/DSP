1cb1f2cc34597ea8705d77c66826d0d64fad7eda
Francis Gurr <f.b.gurr@durham.ac.uk>
2020-01-24 11:39:10 +0000

    Create fir_alt.h

---
+++ src/fir_alt.h
@@ -0,0 +1,4 @@
+#include "structs.h"
+void add_to_buffer(float p_elem, struct Buffer *buff)
+float get_fir_sum(const float *p_H, struct Buffer *buff)
+void fir_alt(float *p_batch, int p_batch_size, const float *p_H, struct Buffer *p_fir_buff)
