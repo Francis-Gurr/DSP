d728993cf4e22ade37fa32571f62868cdb18d392
lmiller1996 <58821699+lmiller1996@users.noreply.github.com>
2020-01-30 14:32:29 +0000

    Add files via upload

--- src/h/init.h
+++ src/h/init.h
@@ -1,3 +1,1 @@
-void get_H_SUM(double *p_H_SUM);
-void get_H_DIFF(double *p_H_DIFF);
-void get_H_RES(const float *p_H0, const float *p_H1, const float *p_H2);
+void get_H_SUM(const double *p_H_SUM);void get_H_DIFF(const double *p_H_DIFF);void get_H_RES(const double *p_H0, const double *p_H1, const double *p_H2);
