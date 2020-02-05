7d51174ae1a1183b27e3cdcf21545abea55c9b1c
vickymiles <58821656+vickymiles@users.noreply.github.com>
2020-02-05 20:36:05 +0000

    Add files via upload

--- src/h/getH_time.m
+++ src/h/getH_time.m
@@ -1,15 +1,5 @@
-% Batch Size * 6/625 = int
-L = 1875;
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% CREATE FILTERS
-function H = getH(Fs, N, Fc, Beta)
-	% All frequency values are in MHz.
-	% Create the window vector for the design algorithm.
-	win = kaiser(N+1, Beta);
-	% Calculate the coefficients using the FIR1 function.
-	H  = fir1(N, Fc/(Fs/2), 'low', win, 'scale');
-end
+%% CHOOSE FILTERS
 
 % Choose from: 16, 32, 64, 128, 256, 512
 N_LOW_1 = 32;
@@ -17,11 +7,13 @@ N_LOW_2 = 128;
 N_RES =   256;
 Beta = 4.54;     % Window Parameter (4.54, 6.76, 8.96)
 
-H_LOW_1 = getH(5, N1, 2, Beta);
-H_LOW_2 = getH(1, N2, 0.05, Beta);
-H_RES = getH(6, (N_RES*6), ?, Beta);
-H_RES = reshape(H_RES, [6, N_RES]);
+H_LOW_1 = getH(5, (N_LOW_1-1), 2, Beta);
+H_LOW_2 = getH(1, (N_LOW_2-1), 0.05, Beta);
+H_RES = getH(6, ((N_RES*6)-1), 0.025, Beta);
+H_RES = reshape(H_RES,[6,N_RES]);
 
+% Batch Size * 6/625 = int
+L = 1875;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% CREATE init.c FILE
 fileID = fopen('init.c','w');
@@ -34,7 +26,7 @@ for i = 1:N_LOW_1-1
         fprintf(fileID,'\n\t\t');
     end
 end
-fprintf(fileID,'%.15f,};\n\n', H_LOW_1(N_LOW_1));
+fprintf(fileID,'%.15f};\n\n', H_LOW_1(N_LOW_1));
 
 % H_LOW_2
 fprintf(fileID,'const double H_LOW_2[%d] = {', N_LOW_2);
@@ -44,7 +36,7 @@ for i = 1:N_LOW_2-1
         fprintf(fileID,'\n\t\t');
     end
 end
-fprintf(fileID,'%.15f,};\n\n', H_LOW_2(N_LOW_2));
+fprintf(fileID,'%.15f};\n\n', H_LOW_2(N_LOW_2));
 
 % H_RES
 fprintf(fileID,'const double H_RES[6][%d] = {', N_RES);
@@ -57,7 +49,7 @@ for i = 1:6
         end
     end
     if i==6
-        fprintf(fileID,'%.15f}};\n\n', H_RES(i, N_RES));
+        fprintf(fileID,'%.15f}};', H_RES(i, N_RES));
     else
         fprintf(fileID,'%.15f},\n\t\t', H_RES(i, N_RES));
     end
@@ -72,14 +64,24 @@ fileID = fopen('init.h','w');
 fprintf(fileID,'#ifndef _CONSTS\n');
 fprintf(fileID,'#define _CONSTS\n\n');
 fprintf(fileID,'#define L %d\n', L);
-fprintf(fileID, '#define L2 %d\n', (L *2));
+fprintf(fileID, '#define L2 %d\n', (L*2));
 fprintf(fileID,'#define M1 %d\n', N_LOW_1);
 fprintf(fileID,'#define M2 %d\n', N_LOW_2);
 fprintf(fileID,'#define M_RES %d\n', N_RES);
-fprintf(fileID,'#define SIZE_OUT %d\n', (L * 6/625));
-fprintf(fileID, '\nconst double H_LOW_1[%d];\n', N_LOW_1);
-fprintf(fileID, '\nconst double H_LOW_2[%d];\n', N_LOW_2);
-fprintf(fileID, '\nconst double H_RES[6][%d];\n', N_RES);
+fprintf(fileID,'#define SIZE_OUT %d\n', (L*6/625));
+fprintf(fileID, '\nconst double H_LOW_1[M1];\n');
+fprintf(fileID, '\nconst double H_LOW_2[M2];\n');
+fprintf(fileID, '\nconst double H_RES[6][M_RES];\n');
 fprintf(fileID,'\n#endif\n');
 
 fclose(fileID);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% CREATE FILTERS
+function H = getH(Fs, N, Fc, Beta)
+	% All frequency values are in MHz.
+	% Create the window vector for the design algorithm.
+	win = kaiser(N+1, Beta);
+	% Calculate the coefficients using the FIR1 function.
+	H  = fir1(N, Fc/(Fs/2), 'low', win, 'scale');
+end
--- src/h/init.h
+++ src/h/init.h
@@ -1,17 +1,17 @@
 #ifndef _CONSTS
 #define _CONSTS
 
-#define N 0
 #define L 1875
 #define L2 3750
-#define M 512
-#define M_RES 1024
+#define M1 32
+#define M2 128
+#define M_RES 256
 #define SIZE_OUT 18
 
-const double H_LOW[512];
+const double H_LOW_1[M1];
 
-const double H_RES[6][1024];
+const double H_LOW_2[M2];
 
-const double OSC[200];
+const double H_RES[6][M_RES];
 
 #endif
