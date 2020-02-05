ad7cf0d80e8f0d1bf72670ad04695ea02d0b5adc
Francis Gurr <f.b.gurr@durham.ac.uk>
2020-02-05 01:06:53 +0000

    Add files via upload

--- src/h/init.h
+++ src/h/init.h
@@ -2,14 +2,14 @@
 #define _CONSTS
 
 #define N 174
-#define L 347
-#define M 347
+#define L 625
+#define M 625
 #define M_RES 1024
-#define SIZE_OUT 1.670400e+00
+#define SIZE_OUT 6
 
 const double H_DEMOD[3][10];
 
-const double H_LOW[347];
+const double H_LOW[625];
 
 const double H_RES[6][1024];
 
