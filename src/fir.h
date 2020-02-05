e20139636e48ddc67577ef3acb8b16e7f505f495
Francis Gurr <francis.gurr@gmail.com>
2020-02-05 01:06:14 +0000

    oops

--- src/fir.h
+++ src/fir.h
@@ -1,5 +1,2 @@
-#include "structs.h"
-#include<stdio.h>
-#include<time.h>
-#include"h/init_600.h"
-void fir(FILE *fd, double *p_batch_out, const int batch_size, int filter_id, struct Buffer *p_buff);
+#include"h/init.h"
+void fir(double *p_sum, double *p_diff, double *p_buff_sum, double *p_buff_diff, int *p_offset);
