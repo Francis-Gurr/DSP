e62bab0387fe388e3c775b9189dd876fc532aef1
vickymiles <58821656+vickymiles@users.noreply.github.com>
2020-02-05 18:16:59 +0000

    Add files via upload

--- src/h/init.h
+++ src/h/init.h
@@ -1,16 +1,16 @@
 #ifndef _CONSTS
 #define _CONSTS
 
-#define N 8192
-#define L 7500
-#define L2 15000
-#define M 693
+#define N 4096
+#define L 3750
+#define L2 7500
+#define M 347
 #define M_RES 1024
-#define SIZE_OUT 72
+#define SIZE_OUT 36
 
 const double H_DEMOD[3][100];
 
-const double H_LOW[8192][2];
+const double H_LOW[4096][2];
 
 const double H_RES[6][1024];
 
