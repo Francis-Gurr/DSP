36d1d366ec3e7aa96fecd18355cd392f2a5cfc5d
vickymiles <58821656+vickymiles@users.noreply.github.com>
2020-02-06 01:13:46 +0000

    Add files via upload

--- src/h/init.h
+++ src/h/init.h
@@ -4,9 +4,9 @@
 #define L 1875
 #define L2 3750
 #define L_2 75
-#define M1 16
-#define M2 16
-#define M_RES 16
+#define M1 512
+#define M2 512
+#define M_RES 512
 #define SIZE_OUT 18
 
 const double H_LOW_1[M1];
